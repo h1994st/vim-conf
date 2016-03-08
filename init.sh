3accb9071a92484425fe5a63711a566b5e26adfa
h1994st <h1994st@gmail.com>
2016-03-08 13:16:28 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,13 @@
-git clone https://github.com/VundleVim/Vundle.vim.git bundle/Vundle.vim
+# Install dependencies
+SYSTEM=`uname -s`
+if [SYSTEM = "Linux"] ; then
+    echo "Linux"
+    sudo apt-get install git vim ctags
+fi
+
+# Clone Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install plugins
 vim +PluginInstall +qall
 echo 'Done!'
